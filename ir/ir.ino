09d5de35ab55abc0e8058b621ae231b7400e682a
TommasoTodescato <tommasotodescato@gmail.com>
2024-03-31 17:27:38 +0200

    ir now prints most changed value

--- ir/ir.ino
+++ ir/ir.ino
@@ -1,17 +1,58 @@
+#include <limits.h>
+
+#define SENSORS 16
+
+int start_time;
+int start[SENSORS], min[SENSORS];
+
+int get_max(int arr[], int size)
+{
+  int index = 0, m = INT_MIN;
+  for (int i = 0; i < size; i++)
+  {
+    if (arr[i] > m)
+    {
+      m = arr[i];
+      index = i;
+    }
+  }
+  return index;
+}
+
 void setup()
 {
   Serial.begin(750);
+  start_time = millis();
+
+  for (int i = 0; i < SENSORS; i++)
+    start[i] = min[i] = analogRead(i);
 }
 
 void loop()
 {
-  delay(500);
-
-  int m = 1024;
-  for (int i = 0; i < 16; i++)
+  for (int i = 0; i < SENSORS; i++)
   {
     int tmp = analogRead(i);
-    m = min(m, tmp);
+    if (tmp < min[i])
+      min[i] = tmp;
   }
-  Serial.println(m);
+
+  int t = millis();
+  if (t - start_time >= 1000)
+  {
+    start_time = t;
+
+    int diff[SENSORS];
+    for (int i = 0; i < SENSORS; i++)
+      diff[i] = start[i] - min[i];
+
+    int max_index = get_max(diff, SENSORS);
+
+    Serial.print(max_index);
+    Serial.print(" / ");
+    Serial.println(diff[max_index]);
+    
+    for (int i = 0; i < SENSORS; i++)
+      start[i] = min[i] = analogRead(i);
+  }
 }
