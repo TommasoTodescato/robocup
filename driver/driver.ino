e7a34d2caf541de771339d5e222c901f4c75c00b
TommasoTodescato <tommasotodescato@gmail.com>
2024-04-02 12:49:35 +0200

    driver progress

--- driver/driver.ino
+++ driver/driver.ino
@@ -1,49 +1,70 @@
-int AIN1 = 2;
-int AIN2 = 3;
-int STBY = 4;
-int BIN1 = 5;
-int BIN2 = 6;
-int PWMA = 10;  // enable pin 1
-int PWMB = 11;  // enable pin 2
-// D2 and D3 as a set, D5 and D6 as a set
-// D10 and D11 as enable pins for DC motors
+// driver A (destra)
+const int A_PWMA = 7;
+const int A_AIN1 = 8;   // motore 1
+const int A_AIN2 = 9;   // --------
+const int A_STBY = 10;
+const int A_BIN1 = 11;  // motore 2
+const int A_BIN2 = 12;  // --------
+const int A_PWMB = 13;
+
+// driver B (sinistra)
+const int B_PWMA = 2;
+const int B_STBY = 3;
+const int B_BIN1 = 4;   // motore 3
+const int B_BIN2 = 5;   // --------
+const int B_PWMB = 6;
+
+// id motori:
+//  driver A motore A -> 1
+//  driver A motore B -> 2
+//  driver B motore B -> 3
+
+bool gira(int id, bool senso, int potenza, int millis)
+{
+  switch(id)
+  {
+    case 1:
+      digitalWrite(A_STBY, HIGH);
+      digitalWrite(A_AIN1, senso);
+      digitalWrite(A_AIN2, !senso);
+      analogWrite(A_PWMA, potenza);
+      delay(millis);
+      digitalWrite(A_STBY, LOW);
+      break;
+
+    case 2:
+      digitalWrite(A_STBY, HIGH);
+      digitalWrite(A_BIN1, senso);
+      digitalWrite(A_BIN2, !senso);
+      analogWrite(A_PWMB, potenza);
+      delay(millis);
+      digitalWrite(A_STBY, LOW);
+      break;
+
+    case 3:
+      digitalWrite(B_STBY, HIGH);
+      digitalWrite(B_BIN1, senso);
+      digitalWrite(B_BIN2, !senso);
+      analogWrite(B_PWMB, potenza);
+      delay(millis);
+      digitalWrite(B_STBY, LOW);
+      break;
+
+    default:
+      return false;
+  }
+
+  return true;
+}
+
 void setup()
 {
-  int i;
-  for (i = 2; i <= 6; i++)  // Ardunio motor driver module
-    pinMode(i, OUTPUT);     // set digital pins 2,3,4,5,6 as output
-
-  pinMode(10, OUTPUT);      // set digital pins 10, 11 as output
-  pinMode(11, OUTPUT);
+  for (int i = 2; i < 14; i++)
+    pinMode(i, OUTPUT);
 }
 void loop()
 {
-  // 2 DC motor rotate CW for 1S, and rotate CCW for 1S
-  // rotate CW
-  digitalWrite(STBY, HIGH);
+  gira(3, true, 255, 3000);
 
-  digitalWrite(AIN1, HIGH);
-  digitalWrite(AIN2, LOW);
-  analogWrite(PWMA, 200);
-
-  digitalWrite(BIN1, HIGH);
-  digitalWrite(BIN2, LOW);
-  analogWrite(PWMB, 200);
-  delay(1000);
-
-  digitalWrite(STBY, LOW);
-  delay(1000);
-  // rotate CCW
-  digitalWrite(STBY, HIGH);
-  digitalWrite(AIN1, LOW);
-  digitalWrite(AIN2, HIGH);
-  analogWrite(PWMA, 100);
-
-  digitalWrite(BIN1, LOW);
-  digitalWrite(BIN2, HIGH);
-  analogWrite(PWMB, 100);
-  delay(1000);
-  
-  digitalWrite(STBY, LOW);
-  delay(1000);
+  delay(5000);
 }
